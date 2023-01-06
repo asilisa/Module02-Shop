ff658f22f70bd40454f897bf07fa641200757e5e
asilisa <asya.mogiley@gmail.com>
2023-01-06 15:10:59 +0300

    add all sections and almost pixelperfect

--- src/index.js
+++ src/index.js
@@ -1,2 +1,63 @@
 import 'normalize.css'
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 import './styles/main.scss';
+
+import Swiper, { Navigation, Pagination } from 'swiper';
+
+
+
+var swiper = new Swiper(".offerSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination],
+    breakpoints: {
+        // when window width is >= 320px
+        320: {
+          slidesPerView: 1,
+        //   spaceBetween: 20,
+          slidesPerGroup: 1
+        },
+        768: {
+            slidesPerView: 2,
+          //   spaceBetween: 20,
+            slidesPerGroup: 2
+        },
+        1000: {
+            slidesPerView: 3,
+          //   spaceBetween: 20,
+            slidesPerGroup: 3
+          },
+        
+      }
+  });
+
+  var swiper = new Swiper(".trendsSwiper", {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    slidesPerGroup: 3,
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    modules: [Navigation, Pagination]
+    
+  });
